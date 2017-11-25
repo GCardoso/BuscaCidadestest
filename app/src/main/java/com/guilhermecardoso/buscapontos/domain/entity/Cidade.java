63a2127304b5ca6b20a80eb476891e184239c331
Guilherme Cardoso <gcardoso18@gmail.com>
2017-11-25 10:50:13 -0200

    Adding retrofit service, interceptor and entity

--- app/src/main/java/com/guilhermecardoso/buscapontos/domain/entity/Cidade.java
+++ app/src/main/java/com/guilhermecardoso/buscapontos/domain/entity/Cidade.java
@@ -1,8 +1,36 @@
 package com.guilhermecardoso.buscapontos.domain.entity;
 
+import com.google.gson.annotations.SerializedName;
+
 /**
  * Created by guilhermecardoso on 11/25/17.
  */
 
 public class Cidade {
+
+    @SerializedName("Nome")
+    private String nome;
+    @SerializedName("Estado")
+    private String estado;
+
+    public Cidade(String nome, String estado) {
+        this.nome = nome;
+        this.estado = estado;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
 }
--- app/src/main/java/com/guilhermecardoso/buscapontos/domain/network/BuscaPontosAPIService.java
+++ app/src/main/java/com/guilhermecardoso/buscapontos/domain/network/BuscaPontosAPIService.java
@@ -1,8 +1,21 @@
 package com.guilhermecardoso.buscapontos.domain.network;
 
+import com.guilhermecardoso.buscapontos.domain.entity.Cidade;
+
+import java.util.List;
+
+import io.reactivex.Observable;
+import retrofit2.http.GET;
+
 /**
  * Created by guilhermecardoso on 11/25/17.
  */
 
-public class BuscaPontosAPIService {
+public interface BuscaPontosAPIService {
+    String SERVICE_ENDPOINT = "http://wsteste.devedp.com.br/Master/CidadeServico.svc/rest/";
+    String SERVICE_TOKEN = null;
+
+    @GET("BuscaTodasCidades/")
+    Observable<List<Cidade>> listCidades();
+    
 }
